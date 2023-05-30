a86579654813b8e12ad3f480dcca937c267f3836
dzhong2 <dzhong2@stevens.edu>
2023-05-30 14:46:33 -0400

    GCN

--- pygcn/models.py
+++ pygcn/models.py
@@ -8,46 +8,28 @@ import scipy.sparse as sp
 
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout, nhead=0,
-                 DP=False, lbd=0,
-                 Ptb=False, gamma=torch.inf,
-                 gpu=True,
-                 ptb_time=-1):
+                 FairDefense=False, gamma=torch.inf,
+                 gpu=True):
         super(GCN, self).__init__()
         self.gpu=gpu
 
         self.gc1 = GraphConvolution(nfeat, nhid)
         self.gc2 = GraphConvolution(nhid, nclass)
         self.dropout = dropout
-        self.DP_layer = DP_layer(lbd)
         self.ptb_adj = perturb_adj(gamma)
         self.adj_ptb = None
-        self.ptb_time = ptb_time
         self.fix_adj = None
-        if lbd <= 0:
-            self.DP = False
-        else:
-            print("Model will go with baseline\nlambda={}\n".format(lbd))
-            self.DP = DP
 
         if gamma == torch.inf:
             self.Ptb = False
         else:
-            print("Model will go with M-In\nGamma={}\n".format(gamma))
-            self.Ptb = Ptb
-            if self.ptb_time:
-                print("Testing! The Adj will only be changed for once")
+            print("Model will go with FairDefense\nGamma={}\n".format(gamma))
+            self.FairDefense = FairDefense
 
     def forward(self, x, adj):
         adj_copy = adj.clone()
         if self.Ptb:
             adj_copy = self.ptb_adj(adj)
-            if self.ptb_time == 1:
-                if self.fix_adj is None:
-                    self.fix_adj = adj_copy
-            elif self.ptb_time == 2:
-                adj_copy = self.ptb_adj(self.adj_ptb) if self.adj_ptb is not None else adj_copy
-            else:
-                adj_copy = self.fix_adj
 
         self.adj_ptb = adj_copy
         '''if self.gpu:
@@ -58,6 +40,4 @@ class GCN(nn.Module):
         x = F.relu(self.gc1(x, adj_copy))
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj_copy)
-        if self.DP:
-            x = self.DP_layer(x)
         return F.log_softmax(x, dim=1)
